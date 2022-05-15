9eadd3c6b288c104bbfa752951cc416727f9211e
mosaelkihal <mosaelkihal@sqli.com>
2022-05-15 18:52:12 +0100

    First Commit

---
+++ src/test/java/com/katas/bankaccountkata/bankaccountkata/BankaccountkataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katas.bankaccountkata.bankaccountkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaccountkataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
